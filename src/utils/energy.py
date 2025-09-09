09547f6fe56911e83e9e5c65cf3729c8fef4a4d6
Alex Berlaga <55417712+alexberlaga@users.noreply.github.com>
2025-09-09 09:58:41 -0500

    Update energy.py

--- src/utils/energy.py
+++ src/utils/energy.py
@@ -274,7 +274,7 @@ def charmm_structure_to_energy(topology: md.Topology, xyz: np.ndarray, nonbonded
     ff_dir = ensure_charmm_ff(ff_version)
     if np.max(compute_all_distances(t)) > 4 * t.top.n_residues ** 0.5:
         raise RuntimeError("Crazy Structure. Could Not Compute Energy")
-    with tempfile.TemporaryDirectory(dir='/project2/andrewferguson/berlaga') as temp_dir:
+    with tempfile.TemporaryDirectory() as temp_dir:
         pdb_file = f'{temp_dir}/temp.pdb'
         structure_file = f"{temp_dir}/structure.gro"
         topology_file = f"{temp_dir}/topol.top"
